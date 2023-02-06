a64f6e2be0c05e55017bc9156797e0d1c2be09ac
mangesh.dhande <mangeshdhande70@gmail.com>
2023-02-07 00:13:01 +0530

    java added

---
+++ AAPractice/src/CalSum.java
@@ -0,0 +1,17 @@
+public class CalSum {
+
+
+    public static void main(String[] args) {
+
+      System.out.println(fun(5));
+
+
+    }
+
+    public static int fun(int n){
+        return n*(n+1)/2;
+
+    }
+
+
+}
